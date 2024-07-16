c6b2a663c72939299a6fc48c3b099825de0af2de
RanaPlaysWithCodes <ranaalmaslukhi@gmail.com>
2024-07-16 15:35:39 +0300

    Add files via upload

---
+++ appAi.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify, render_template
+
+import pickle
+import numpy as np
+app = Flask(__name__, template_folder='template',static_folder='static')
+
+# Load your trained model
+model = pickle.load(open('trainedmodel.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        data = request.json
+        input_features = np.array(data['features']).reshape(1, -1) # type: ignore
+        prediction = model.predict(input_features)
+        return jsonify({'prediction': prediction[0]})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+if __name__ =="__main__":
+    app.run(debug=True)
